dc15b8622e9ffab33eb76cb17c0f7888d681ca90
Srinandini Marpaka <srinandinim@hotmail.com>
2019-03-02 11:07:43 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 objectPosition = Camera.main.WorldToScreenPoint(transform.position);
+        mousePosition.x = mousePosition.x - objectPosition.x;
+        mousePosition.y = mousePosition.y - objectPosition.y;
+        float angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg - 90;
+        if (angle < 60 && angle > -60)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        }
+    }
+}
