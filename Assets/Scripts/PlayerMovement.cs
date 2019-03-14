5c9a10520ab409b32ee82f264adac002eb255d88
Srinandini Marpaka <srinandinim@hotmail.com>
2019-03-14 10:23:37 -0400

    Added tank, fixed player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,16 +2,35 @@
 
 public class PlayerMovement : MonoBehaviour
 {
+    Vector3 mousePosition, objectPosition;
+    float angle;
+
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, 45));
+    }
+
     void Update()
     {
-        Vector3 mousePosition = Input.mousePosition;
-        Vector3 objectPosition = Camera.main.WorldToScreenPoint(transform.position);
+        mousePosition = Input.mousePosition;
+        objectPosition = Camera.main.WorldToScreenPoint(transform.position);
         mousePosition.x = mousePosition.x - objectPosition.x;
         mousePosition.y = mousePosition.y - objectPosition.y;
-        float angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg - 90;
-        if (angle < 60 && angle > -60)
+        angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+        if (angle > 12 && angle < 90)
         {
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-        }
+        } /*else if (angle > 60)
+        {
+            angle = 60;
+        } else if (angle < -60)
+        {
+            angle = -60;
+        }*/
+    }
+
+    public float getAngle()
+    {
+        return angle;
     }
 }
---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+
+    PlayerMovement playerMovement;
+
+    private void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log ("space key was pressed");
+            Debug.Log(playerMovement.getAngle() + "");
+        }
+    }
+}
